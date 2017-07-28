fa847afe17db999a3ade3a70dc03341b96e09312
b1anker <narutowontstop@gmail.com>
2017-07-28 16:11:17 +0800

    Add click event

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -3,8 +3,27 @@ import React, { Component } from 'react';
 import Square from '../square/index'
 
 class Board extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick (i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares})
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={ this.state.squares[i] }
+        onClick={ () => this.handleClick(i) }
+      />
+    );
   }
 
   render() {
